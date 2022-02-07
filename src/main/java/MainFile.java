777db15ab93c6ecb3380c986624149f1bbd12105
hakan <hakanbengu844@hotmail.com>
2022-02-08 00:11:24 +0300

    Removed a println 'F02'.

--- src/main/java/MainFile.java
+++ src/main/java/MainFile.java
@@ -5,7 +5,6 @@ public class MainFile {
 
     static void main(String[] args){
 
-        System.out.println("F02");
         System.out.println("Hello World");
         System.out.println("Hello World");
 
