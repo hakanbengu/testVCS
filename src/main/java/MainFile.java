bd7adf66d90c74cdc76cdae7d6a368c08bdac270
hakan <hakanbengu844@hotmail.com>
2022-02-08 00:09:04 +0300

    Added a println 'F02' temporarily.

--- src/main/java/MainFile.java
+++ src/main/java/MainFile.java
@@ -5,6 +5,7 @@ public class MainFile {
 
     static void main(String[] args){
 
+        System.out.println("F02");
         System.out.println("Hello World");
         System.out.println("Hello World");
 
